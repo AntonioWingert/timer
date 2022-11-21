8ef27a9dc6c2c704bf1215036e99f9544f733442
Antonio Bruno Wingert <antoniobwingert@gmail.com>
2022-11-21 15:52:00 -0300

    update counter test, resolve problem in the count timer

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -5,17 +5,16 @@ import Counter from '../components/Counter/Counter';
 describe('testa se o timer funciona corretamente', () => {
   it('Verifica se ao digitar no input e acionar o timer funciona corretamente', () => {
     render(<Counter />);
-    const DEFAULT_SECONDS = 25;
+    const DEFAULT_SECONDS = '25';
     const timer = screen.getByRole('timer');
     const inputSeconds = screen.getByPlaceholderText('ss');
     const inputMinute = screen.getByPlaceholderText('mm');
     const buttonStart = screen.getByRole('button');
 
-    userEvent.type(inputMinute, 0);
+    userEvent.type(inputMinute, '0');
     userEvent.type(inputSeconds, DEFAULT_SECONDS);
     userEvent.click(buttonStart);
 
-    console.log(timer);
     expect(timer).toHaveTextContent('00:25');
   });
 });
